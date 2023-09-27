b82718e25b5a7f0c21620f913b9f0b8bc62eb3a3
unknown <shantanupawar101@gmail.com>
2023-09-27 19:02:37 +0530

    first commit

---
+++ pf/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Hero from './components/Hero'
+import Who from './components/Who'
+import Works from './components/Works'
+import Contact from './components/Contact'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
