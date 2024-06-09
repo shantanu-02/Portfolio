3d74782f02d5e4d925fb849c5b0b225428e02049
unknown <shantanupawar101@gmail.com>
2024-06-09 19:39:59 +0530

    Updated README.md

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = (props) => {
     <a href={props.link} target="_blank" rel="noopener noreferrer" className="card-link" style={{ textDecoration: 'none', color: 'white' }}>
     <div className='card'>
       <div className='image'>
-        <img src={props.img} className='cardImage'/>
+        <img src={`${props.img}`} className='cardImage'/>
       </div>
         <h1 className="cardTitle">{props.title}</h1>
         <p className="cardInfo">{props.info}</p>
